a45e4df232cbaeeaa916d71914d4b080da1f7542
alexvezeau <alex.vezeau@gmail.com>
2012-09-06 16:13:44 -0400

    Second commit

--- src/com/vezeau/alex/gae/HelloBean.java
+++ src/com/vezeau/alex/gae/HelloBean.java
@@ -1,28 +1,42 @@
 package com.vezeau.alex.gae;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
- 
-import java.io.Serializable;
- 
+
 @ManagedBean
 @SessionScoped
 public class HelloBean implements Serializable {
- 
+
 	private static final long serialVersionUID = 1L;
- 
+
 	private String name;
- 
+
 	public String getName() {
 		return name;
 	}
- 
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getFormattedName(){
+
+	public String getFormattedName() {
+		Dao.INSTANCE.add(name, "summary", "description",
+				String.valueOf(System.currentTimeMillis()));
+
 		return "Hello, " + name + "!";
 	}
- 
+
+	public Todo[] getAllEntries() {
+		List<Todo> todos = Dao.INSTANCE.getTodos(name);
+		
+		return todos.toArray(new Todo[todos.size()]);
+	}
+	
+	public void setAllEntries(Todo[] todos) {
+		
+	}	
+
 }
